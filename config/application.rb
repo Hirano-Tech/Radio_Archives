9bc3ecc0d2be38904e2c4e6e95c3e3de53ad0cd0
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-09 23:12:19 +0000

    アプリケーションの基本設定を編集する。

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,25 @@
-require_relative "boot"
+require_relative 'boot'
+require 'rails/all'
 
-require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module RadioArchives
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Osaka'
+    config.beginning_of_week = :monday
+    config.i18n.default_locale = :ja
+
+    console do
+      require 'pry'
+      config.console = Pry
+    end
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework false
+    end
   end
 end
