b35d10662fab04840cbf982e1d7fa9bff8b61164
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-12 11:29:50 +0900

    プレイヤー詳細ページの処理を編集する。

--- app/controllers/program_selects_controller.rb
+++ app/controllers/program_selects_controller.rb
@@ -1,26 +1,29 @@
 class ProgramSelectsController < ApplicationController
   def index
     unless cookies.signed[:program].blank?
-      Aws.config.update({
-        region: Rails.application.credentials.AWS_S3[:Region],
-        credentials: Aws::Credentials.new(
-          Rails.application.credentials.AWS_S3[:Access_Key_ID],
-          Rails.application.credentials.AWS_S3[:Secret_Access_Key]
-        )
-      })
-
-      @image_url = Aws::S3::Presigner.new.presigned_url(
-        :get_object,
-        bucket: 'radio-archives',
-        key: 'images/BRIDGESTONE DRIVE TO THE FUTURE.webp',
-        expires_in: 10
-      )
-
       @programs = Program.select(:id, :on_air, :already_play).readonly
+
+      aws_credential
+      @image_url = presign_url('images/BRIDGESTONE DRIVE TO THE FUTURE.webp', 60)
     end
   end
 
   def show
+    @program = Program.find(params[:id].to_i)
+
+    aws_credential
+    @image_url = presign_url('images/BRIDGESTONE DRIVE TO THE FUTURE.webp', 60)
+    @audio_url = presign_url(@program.s3_key, 3600)
+  end
+
+  def play
+    if request.post?
+      Program.update(params[:id].to_i, already_play: true)
+      redirect_to program_select_path(id: params[:id].to_i)
+    elsif request.delete?
+      Program.update(params[:id].to_i, already_play: false)
+      redirect_to program_select_path(id: params[:id].to_i)
+    end
   end
 
   def create
@@ -39,6 +42,11 @@ class ProgramSelectsController < ApplicationController
     redirect_to program_selects_path
   end
 
+  def destroy_cookie
+    cookies.signed[:program] = nil
+    redirect_to program_selects_path
+  end
+
   private
   def program_params
     params.permit(:commit)
@@ -46,5 +54,24 @@ class ProgramSelectsController < ApplicationController
 
   def date_params
     params.permit(:broadcast_date)
+  end
+
+  def aws_credential
+    Aws.config.update({
+      region: Rails.application.credentials.AWS_S3[:Region],
+      credentials: Aws::Credentials.new(
+        Rails.application.credentials.AWS_S3[:Access_Key_ID],
+        Rails.application.credentials.AWS_S3[:Secret_Access_Key]
+      )
+    })
+  end
+
+  def presign_url(key, time)
+    Aws::S3::Presigner.new.presigned_url(
+      :get_object,
+      bucket: 'radio-archives',
+      key: key,
+      expires_in: time
+    )
   end
 end
