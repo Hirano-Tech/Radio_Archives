efd817baf8212d5840c83911c5cbb24746691eb4
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-12 18:47:39 +0900

    ログイン機能のためのルーティングを記述する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources(:users, only:[:index, :create, :destroy])
+
   resources(:program_selects, only: [:index, :show, :create])
   match('/program_selects/confirm', to: 'program_selects#confirm', via: :post)
   match('/program_selects/:id/play', to: 'program_selects#play', via: [:post, :delete])
