6c97d202810127212e67a557d7f2c1e609bddff9
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-12 11:28:44 +0900

    再生済みかどうかを処理するためのルーティングを編集する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources(:program_selects, only: [:index, :show, :create])
   match('/program_selects/confirm', to: 'program_selects#confirm', via: :post)
+  match('/program_selects/:id/play', to: 'program_selects#play', via: [:post, :delete])
+  match('/program_selects/destroy', to: 'program_selects#destroy_cookie', via: :delete)
 
   # routes.rb で利用できる詳細については、https://guides.rubyonrails.org/routing.html をご覧ください。
 end
