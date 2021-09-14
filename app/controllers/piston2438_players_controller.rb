9c3c5fb53e99a8a479e4f07cce25617fc6a936f8
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-14 21:29:52 +0900

    現在 再生している曲の ID をセッションから削除するタイミングを変更する。

--- app/controllers/piston2438_players_controller.rb
+++ app/controllers/piston2438_players_controller.rb
@@ -1,5 +1,5 @@
 class Piston2438PlayersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!, only: [:index, :show]
 
   def index
     aws_credential
@@ -8,7 +8,7 @@ class Piston2438PlayersController < ApplicationController
 
   def create
     if params[:play_method] == '全曲シャッフル再生'
-      songs = Program.where(category_id: 2).select(:id).readonly
+      songs = Program.where(category_id: 2).select(:id, :on_air).reorder('on_air DESC').readonly
 
       @play_list = Array.new
       songs.each do |song|
@@ -16,17 +16,29 @@ class Piston2438PlayersController < ApplicationController
       end
 
       session[:play_list] = @play_list.shuffle
-      redirect_to piston2438_player_path(id: session[:play_list].shift)
+      redirect_to piston2438_player_path(id: session[:play_list][0])
     end
   end
 
   def show
-    session[:play_list].shuffle!
+    unless session[:play_list].length == 0
+      aws_credential
+      @image_url = presign_url('images/GROOVE LINE.webp', 60)
 
-    aws_credential
-    @image_url = presign_url('images/GROOVE LINE.webp', 60)
+      @program = Program.select(:id, :s3_key, :on_air, :category_id).readonly.includes(:category).find(params[:id].to_i)
+      @audio_url = presign_url(@program.s3_key, 1800)
 
-    @program = Program.readonly.find(params[:id].to_i)
-    @audio_url = presign_url(@program.s3_key, 1800)
+      session[:play_list].delete_at(0)
+      session[:play_list].shuffle!
+    else
+      redirect_to(root_path)
+    end
   end
+
+  private
+    def authenticate_user!
+      if cookies.signed[:user].blank?
+        redirect_to(users_path)
+      end
+    end
 end
