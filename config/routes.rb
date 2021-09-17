2be9d50bf198a5549124fa47cb2800d84c38c994
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-17 19:04:15 +0900

    ユーザー認証におけるルーティングを編集する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'program_selects#index'
 
-  resources(:users, only:[:index, :create, :destroy])
-  resources(:piston2438_players, only: [:index, :create, :show])
-  resources(:program_selects, only: [:index, :show, :create, :destroy])
-  match('/program_selects/confirm', to: 'program_selects#confirm', via: :post)
-  match('/program_selects/:id/play', to: 'program_selects#play', via: [:post, :delete])
+  resources(:sessions, path: 'user/sessions', only:[:index, :create, :destroy])
+  resources(:registrations, path: 'user/registrations', only:[:index, :new, :create])
+    delete('user/registrations/destroy', to: 'registrations#destroy')
 
+  resources(:program_selects, only: [:index, :show, :create, :destroy])
+    match('/program_selects/confirm', to: 'program_selects#confirm', via: :post)
+    match('/program_selects/:id/play', to: 'program_selects#play', via: [:post, :delete])
+  resources(:piston2438_players, only: [:index, :create, :show])
   # routes.rb で利用できる詳細については、https://guides.rubyonrails.org/routing.html をご覧ください。
 end
