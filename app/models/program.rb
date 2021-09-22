616335adb365a73acbf4ed034291c464bcf90c9a
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-23 03:09:07 +0900

    ピストン西沢 Non-STOP DJ MIX のプレイリストを作成するメソッドを記述する。

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,4 +2,12 @@ class Program < ApplicationRecord
   self.table_name = 'programs'
 
   belongs_to(:category, class_name: 'Category', foreign_key: 'category_id')
+
+  def get_all_mixes
+    return Program.where(category_id: category_id).select(:id, :on_air).reorder('on_air DESC').readonly
+  end
+
+  def get_period_mixes
+    return Program.where(on_air: on_air, category_id: category_id).select(:id, :on_air).reorder('on_air DESC').readonly
+  end
 end
