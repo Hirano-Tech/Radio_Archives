a0154a38206f2f40e419e25d234d2bd00adb377e
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-23 00:15:33 +0900

    User モデルに復号化処理のメソッドを記述する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,16 @@ class User < ApplicationRecord
     return User.connection.select_all("SELECT HEX(AES_ENCRYPT('#{password}', '#{secret_key}'));").rows[0][0]
   end
 
+  def self.decrypt_email(email)
+    secret_key = Rails.application.credentials.User[:Secret_Key]
+    return User.connection.select_all("SELECT convert( AES_DECRYPT( UNHEX('#{email}'), '#{secret_key}') USING 'utf8mb4')").rows[0][0]
+  end
+
+  def self.decrypt_password(password)
+    secret_key = Rails.application.credentials.User[:Secret_Key]
+    return User.connection.select_all("SELECT convert( AES_DECRYPT( UNHEX('#{password}'), '#{secret_key}') USING 'utf8mb4')").rows[0][0]
+  end
+
   def user_sign_in
     return User.readonly.find_by(encrypt_email: encrypt_email, encrypt_password: encrypt_password)
   end
