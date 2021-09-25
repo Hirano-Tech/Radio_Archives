2e126c4a4c91381957f46e67a7525678a2f28396
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-25 10:27:19 +0900

    不要なルーティングを削除する。

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   resources(:registrations, path: 'user/registrations', only:[:index, :new, :create])
     delete('user/registrations/destroy', to: 'registrations#destroy')
 
-  resources(:program_selects, only: [:index, :show, :create, :destroy])
-    match('/program_selects/confirm', to: 'program_selects#confirm', via: :post)
-    match('/program_selects/:id/play', to: 'program_selects#play', via: [:post, :delete])
-  resources(:piston2438_players, only: [:index, :create, :show])
+  resources(:program_selects, only: :index)
+  resource(:players, only: :show)
+
   # routes.rb で利用できる詳細については、https://guides.rubyonrails.org/routing.html をご覧ください。
 end
