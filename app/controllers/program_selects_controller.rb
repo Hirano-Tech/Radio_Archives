edf45d082fbdcd4d1a75ae68efd76a7f3135aa83
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-29 09:04:37 +0900

    FM NUCK5 'FAV FOUR' のアーカイブ配信を追加する。

--- app/controllers/program_selects_controller.rb
+++ app/controllers/program_selects_controller.rb
@@ -2,5 +2,18 @@ class ProgramSelectsController < ApplicationController
   before_action :authenticate_user!, only: :index
 
   def index
+    if params[:program] == 'JODV_FAV-FOUR'
+      client = aws_credential
+      objects = client.list_objects_v2({bucket: Rails.application.credentials.AWS_S3[:Bucket]}).contents
+
+      @programs = Array.new
+      objects.each do |object|
+        object_detail = client.get_object({bucket: Rails.application.credentials.AWS_S3[:Bucket], key: object.key}).to_h
+        if object_detail[:metadata]['program'] == 'FAV FOUR'
+          @programs.push({key: object.key, date: object_detail[:metadata]['date']})
+        end
+      end
+      @programs.reverse!
+    end
   end
 end
