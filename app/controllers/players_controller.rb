4a637e363d48a6bd85a99987972e250cc9768687
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-09-30 19:11:23 +0900

    S3 オブジェクトのメタデータ取得方法を変更する。

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,44 +2,32 @@ class PlayersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    client = aws_credential
-
     if params[:program] == 'JOAV_MIX-MACHINE'
       if cookies.signed[:already].blank?
         cookies.signed[:already] = { value: Array.new(10), expires: 1.day.from_now }
       end
 
-      if Rails.env.production?
+      client = aws_s3_client
+      object = client.list_objects_v2({bucket: Rails.application.credentials.AWS_S3[:Bucket]}).contents.sample
+      object_client = aws_s3_object(object.key)
+
+      already_play = cookies.signed[:already]
+      while object_client.metadata['program'] != 'Piston2438_DJ-MIX' || already_play.include?(object.key)
         object = client.list_objects_v2({bucket: Rails.application.credentials.AWS_S3[:Bucket]}).contents.sample
-        object_detail = client.get_object({bucket: Rails.application.credentials.AWS_S3[:Bucket], key: object.key}).to_h
-
-        already_play = cookies.signed[:already]
-        while object_detail[:metadata]['program'] != 'Piston2438_DJ-MIX' || already_play.include?(object.key)
-          object = client.list_objects_v2({bucket: 'joav.dj-mixes'}).contents.sample
-          object_detail = client.get_object({bucket: 'joav.dj-mixes', key: object.key}).to_h
-        end
-
-        already_play.push(object.key)
-        already_play.shift
-        cookies.signed[:already] = already_play
-
-        @audio_url = "https://d1tr04ubbr03km.cloudfront.net/#{object.key}"
-        @date = Date.parse(object_detail[:metadata]['date']).strftime('%Y年 %m月 %d日')
-      elsif Rails.env.development?
-        # @audio_url = 'https://d1tr04ubbr03km.cloudfront.net/.mp3'
-        # @date = Date.parse('').strftime('%Y年 %m月 %d日')
+        object_client = aws_s3_object(object.key)
       end
+
+      already_play.push(object.key)
+      already_play.shift
+      cookies.signed[:already] = already_play
+
+      @audio_url = "https://d1tr04ubbr03km.cloudfront.net/#{object.key}"
+      @date = Date.parse(object_client.metadata['date']).strftime('%Y年 %m月 %d日')
 
     elsif params[:program] == 'JODV_FAV-FOUR'
       @program = Program.create_or_find_by(s3_key: params[:key])
       @audio_url = "https://d1tr04ubbr03km.cloudfront.net/#{params[:key]}"
-
-      if Rails.env.production?
-        object_detail = client.get_object({bucket: Rails.application.credentials.AWS_S3[:Bucket], key: params[:key]}).to_h
-        @date = Date.parse(object_detail[:metadata]['date']).strftime('%Y年 %m月 %d日')
-      elsif Rails.env.development?
-        # @date = Date.parse('').strftime('%Y年 %m月 %d日')
-      end
+      @date = Date.parse(aws_s3_object(params[:key]).metadata['date']).strftime('%Y年 %m月 %d日')
     end
   end
 
