737b90bc2dd950ec4e973a9551b5b22cdc5484c9
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-10-18 22:37:39 +0900

    過去 数ヶ月分に絞って再生する機能を追加する。

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,19 +8,31 @@ class PlayersController < ApplicationController
       cookies.signed[:already] = { value: Array.new(4), expires: 1.day.from_now }
     end
 
-    client = aws_s3_client
-    object = client.list_objects_v2({bucket: Rails.application.credentials.AWS_S3[:Bucket]}).contents.sample
-    object_client = aws_s3_object(object.key)
-
     already_play = cookies.signed[:already]
-    play_song = Piston2438.repeat_shuffle(client, object, object_client, params[:play], already_play)
+    client = aws_s3_client
+    objects = client.list_objects_v2({bucket: Rails.application.credentials.AWS_S3[:Bucket]}).contents
+    objects.delete_if do |object|
+      object_client = aws_s3_object(object.key)
 
-    already_play.push(play_song[:object].key)
+      program = object_client.metadata['program'] != 'Piston2438_DJ-MIX'
+      if params[:play] == 'shuffle_play'
+        program || already_play.include?(object.key)
+      elsif params[:play] == 'one_month'
+        program || Date.parse(object_client.metadata['date']) < Date.today.months_ago(1) || already_play.include?(object.key)
+      elsif params[:play] == 'three_months'
+        program || Date.parse(object_client.metadata['date']) < Date.today.months_ago(3) || already_play.include?(object.key)
+      end
+    end
+
+    play_song = objects.sample
+
+    already_play.push(play_song.key)
     already_play.shift
     cookies.signed[:already] = already_play
 
-    @audio_url = "https://d1tr04ubbr03km.cloudfront.net/#{play_song[:object].key}"
-    @date = Date.parse(play_song[:object_client].metadata['date']).strftime('%Y年 %m月 %d日')
+    @audio_url = "https://d1tr04ubbr03km.cloudfront.net/#{play_song.key}"
+    object_client = aws_s3_object(play_song.key)
+    @date = Date.parse(object_client.metadata['date']).strftime('%Y年 %m月 %d日')
   end
 
   def update
